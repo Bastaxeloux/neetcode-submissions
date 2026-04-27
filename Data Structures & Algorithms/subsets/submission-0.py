f0a3dc70d6a1786ca0020dee210432a46c476ec1
MLG <76120242+Bastaxeloux@users.noreply.github.com>
2026-04-27 18:02:42 +0200

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,27 @@
+def subsets_of_size(nums,i):
+    n = len(nums)
+    subsets_of_size_i = []
+    if i == 1:
+        for num in nums :
+            subsets_of_size_i.append([num])
+    elif 1<i<=n: # pour les subsets de taille 2 à n
+        for j,num in enumerate(nums[0:n-i+1]): # on stop à n-i
+            temp = subsets_of_size(nums[j+1:],i-1)
+            for subs in temp:
+                subs.extend([num])
+                subsets_of_size_i.append(subs)
+    print(f"call sur i={i} et subsets = {subsets_of_size_i}")
+    return subsets_of_size_i 
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subsets = []
+        subsets.append([])
+        n = len(nums)
+        if n == 0 :
+            return []
+        for i in range (1,n+1):
+            # goal is to find all subsets of size i here
+            subsets.extend(subsets_of_size(nums,i))
+            print(subsets)
+        return subsets
