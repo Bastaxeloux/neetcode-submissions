8f85cf0e4c1848490ee87647e0a30cf6ffad9703
MLG <76120242+Bastaxeloux@users.noreply.github.com>
2026-08-11 12:03:02 +0200

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0 : return 0
+        nums.sort()
+        diff = [nums[i]-nums[i-1] for i in range(1,len(nums))]
+        diff_filt = [num for num in diff if num!=0]
+        n, temp = 0,0
+        for i in range(len(diff_filt)):
+            if diff_filt[i] == 1:
+                temp += 1
+            else :
+                n = max(n,temp)
+                temp = 0
+        n = max(n,temp)
+        return n+1
